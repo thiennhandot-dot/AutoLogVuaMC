7b15eef06f972982ea5022ac869d85f07ee1e83c
thiennhandot-dot <thiennhandot@gmail.com>
2026-03-09 04:03:39 +0700

    Update ModuleExample.java

--- src/main/java/com/example/addon/modules/ModuleExample.java
+++ src/main/java/com/example/addon/modules/ModuleExample.java
@@ -14,7 +14,6 @@ public class ModuleExample extends Module {
 
     private final Setting<String> password = sgGeneral.add(new StringSetting.Builder()
         .name("mat-khau")
-        .description("Mat khau VuaMC")
         .defaultValue("123456")
         .build()
     );
@@ -37,12 +36,14 @@ public class ModuleExample extends Module {
         if (mc.player == null) return;
         timer++;
 
+        // Tu dong dang nhap sau 3 giay
         if (!daLogin && timer >= 60) {
             ChatUtils.sendPlayerMsg("/login " + password.get());
             daLogin = true;
         }
 
-        if (daLogin && timer == 100) {
+        // Tu dong mo La ban sau khi login
+        if (daLogin && timer == 120) {
             for (int i = 0; i < 9; i++) {
                 if (mc.player.getInventory().getStack(i).getItem() == Items.COMPASS) {
                     mc.player.getInventory().selectedSlot = i;
