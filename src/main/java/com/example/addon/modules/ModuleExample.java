a55a6bfd1f940c2d8b260eb02a9316ab0a00164c
thiennhandot-dot <thiennhandot@gmail.com>
2026-03-09 03:43:38 +0700

    Update ModuleExample9.java

--- src/main/java/com/example/addon/modules/ModuleExample.java
+++ src/main/java/com/example/addon/modules/ModuleExample.java
@@ -1,9 +1,12 @@
 package com.example.addon.modules;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.StringSetting;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
@@ -13,6 +16,7 @@ public class ModuleExample extends Module {
 
     private final Setting<String> password = sgGeneral.add(new StringSetting.Builder()
         .name("mat-khau")
+        .description("Mat khau cua ban")
         .defaultValue("123456")
         .build()
     );
@@ -21,7 +25,7 @@ public class ModuleExample extends Module {
     private boolean daLogin;
 
     public ModuleExample() {
-        super(Categories.Misc, "AutoLogVuaMC", "Mod by Kadeer");
+        super(Categories.Misc, "AutoLogVuaMC", "Auto login - Mod by Kadeer");
     }
 
     @Override
@@ -35,16 +39,20 @@ public class ModuleExample extends Module {
         if (mc.player == null) return;
         timer++;
 
+        // Su dung ChatUtils cua Meteor de KHONG BI LOI phien ban MC
         if (!daLogin && timer >= 60) {
-            mc.player.networkHandler.sendChatCommand("login " + password.get());
+            ChatUtils.sendPlayerMsg("/login " + password.get());
             daLogin = true;
         }
 
+        // Sau khi login 2 giay, tu dong tim va bam La ban
         if (daLogin && timer == 100) {
             for (int i = 0; i < 9; i++) {
                 if (mc.player.getInventory().getStack(i).getItem() == Items.COMPASS) {
                     mc.player.getInventory().selectedSlot = i;
-                    mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
+                    if (mc.interactionManager != null) {
+                        mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
+                    }
                     break;
                 }
             }
