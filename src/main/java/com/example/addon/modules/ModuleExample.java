9ff2807aa76f93e351a3b5c3909b3a9c46f1eb40
thiennhandot-dot <thiennhandot@gmail.com>
2026-03-09 03:28:06 +0700

    Update ModuleExample6.java

--- src/main/java/com/example/addon/modules/ModuleExample.java
+++ src/main/java/com/example/addon/modules/ModuleExample.java
@@ -8,19 +8,24 @@ import meteordevelopment.orbit.EventHandler;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 
+/* * PROJECT: AutoLogVuaMC x Camdzs1tg
+ * WARNING: Thang nao mo code nay ra xem la con cho :))
+ */
+
 public class ModuleExample extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<String> password = sgGeneral.add(new StringSetting.Builder()
         .name("mat-khau")
         .defaultValue("123456")
-        .build());
+        .build()
+    );
 
     private int timer;
     private boolean daLogin;
 
     public ModuleExample() {
-        super(Categories.Misc, "AutoLogVuaMC", "Mod by Kadeer");
+        super(Categories.Misc, "AutoLogVuaMC", "Auto login VuaMC - Mod by Kadeer");
     }
 
     @Override
@@ -34,12 +39,14 @@ public class ModuleExample extends Module {
         if (mc.player == null) return;
         timer++;
 
-        if (!daLogin && timer >= 40) {
+        // Tu dong gui lenh login sau 3 giay (60 ticks)
+        if (!daLogin && timer >= 60) {
             mc.player.networkHandler.sendChatCommand("login " + password.get());
             daLogin = true;
         }
 
-        if (daLogin && timer == 80) {
+        // Tu dong cam la ban va click chuot phai sau khi login
+        if (daLogin && timer == 100) {
             for (int i = 0; i < 9; i++) {
                 if (mc.player.getInventory().getStack(i).getItem() == Items.COMPASS) {
                     mc.player.getInventory().selectedSlot = i;
@@ -49,4 +56,4 @@ public class ModuleExample extends Module {
             }
         }
     }
-}
+            }
